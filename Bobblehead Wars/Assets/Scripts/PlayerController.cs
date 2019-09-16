2f91f23206b3634325da6531d46693b2e7b5bee4
Mike Sullivan <mjes87@gmail.com>
2019-09-16 15:20:21 -0400

    Joined Head to Body
    Added hinge and rigidbody to SpaceMarine for bobbleheaded effect

--- Bobblehead Wars/Assets/Scripts/PlayerController.cs
+++ Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    public Rigidbody head;
 
     private CharacterController characterController;
 
@@ -20,5 +21,20 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    // Called on a fixed interval to process physics
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            // TODO
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
