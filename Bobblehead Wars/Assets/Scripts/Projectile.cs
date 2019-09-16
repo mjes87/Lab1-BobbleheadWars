0fdf06d1f28b3666352cd89eaac1750a10ad8677
Mike Sullivan <mjes87@gmail.com>
2019-09-16 14:21:40 -0400

    Added Projectile Destruction
    Projectiles are now destroyed when they leave screen or contact a collider

---
+++ Bobblehead Wars/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Destroys projectiles when they are no longer visible on screen
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // Destroys projectiles when they come into contact with a collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
