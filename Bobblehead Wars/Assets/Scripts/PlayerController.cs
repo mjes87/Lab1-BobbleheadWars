aa190673503e7e3cdf6be22b02e9147b352aa3e7
Mike Sullivan <mjes87@gmail.com>
2019-09-16 16:01:29 -0400

    Added Raycasting to marine
    Added Raycasting functionality to SpaceMarine. Now turns following the cursor.

--- Bobblehead Wars/Assets/Scripts/PlayerController.cs
+++ Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     public Rigidbody head;
+    public LayerMask layerMask;
 
+    private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
 
     // Start is called before the first frame update
@@ -27,7 +29,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
-0, Input.GetAxis("Vertical"));
+            0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
             // TODO
@@ -36,5 +38,32 @@ public class PlayerController : MonoBehaviour
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
-    }
+
+        // Creates a Ray and casts it to the mouse position
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Makes the Ray visible
+        //Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        // Casts the ray and attempts to hit
+        if (Physics.Raycast(ray, out hit, 1000, layerMask,
+            QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+
+            // 1 - Gets the target position
+            Vector3 targetPosition = new Vector3(hit.point.x,
+            transform.position.y, hit.point.z);
+            // 2 - Gets current quaternion and determines which way the marine should look
+            Quaternion rotation = Quaternion.LookRotation(targetPosition -
+            transform.position);
+            // 3 - Does the actual turning
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+            rotation, Time.deltaTime * 10.0f);
+        }
+    }   
 }
