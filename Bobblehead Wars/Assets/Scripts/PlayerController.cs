77c9abc497daf537a2557504e9027a08b9e15606
Mike Sullivan <mjes87@gmail.com>
2019-10-31 15:10:45 -0400

    Added ScreenShake
    Screen now shakes and deducts HP from Player when they are contacted by an enemy

--- Bobblehead Wars/Assets/Scripts/PlayerController.cs
+++ Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,14 @@ public class PlayerController : MonoBehaviour
     public Rigidbody head;
     public LayerMask layerMask;
     public Animator bodyAnimator;
+    public float[] hitForce;
+    public float timeBetweenHits = 2.5f;
 
     private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
+    private bool isHit = false;
+    private float timeSinceHit = 0;
+    private int hitNumber = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +29,41 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+
+        if (isHit)
+        {
+            timeSinceHit += Time.deltaTime;
+            if (timeSinceHit > timeBetweenHits)
+            {
+                isHit = false;
+                timeSinceHit = 0;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Alien alien = other.gameObject.GetComponent<Alien>();
+        if (alien != null)
+        { // 1
+            if (!isHit)
+            {
+                hitNumber += 1; // 2
+                CameraShake cameraShake = Camera.main.GetComponent<CameraShake>();
+                if (hitNumber < hitForce.Length) // 3
+                {
+                    cameraShake.intensity = hitForce[hitNumber];
+                    cameraShake.Shake();
+                }
+                else
+                {
+                    // death todo
+                }
+                isHit = true; // 4
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
+            }
+            alien.Die();
+        }
     }
 
     // Called on a fixed interval to process physics
